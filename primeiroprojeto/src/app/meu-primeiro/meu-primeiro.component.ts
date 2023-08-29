e61bf6555344e4fd54e2bf5910138bfa690a6fdf
Lucas-1450 <84975789+Lucas-1450@users.noreply.github.com>
2023-08-28 23:23:40 -0300

    Add files via upload

---
+++ primeiroprojeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <h4>Meu primeiro component com Angular 2!</h4>
+    `
+})
+export class MeuPrimeiroComponent {}
